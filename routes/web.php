041f5f0febb75b02b596614a36150daed770e936
Rajabov Diyorbek <rajabov.diyorbek.it@gmail.com>
2026-07-06 15:40:01 +0500

    learn Routing

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,37 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $person = [
-        'name'=> 'Diyorbek',
-        'email'=> 'rajabov.diyorbek.it@gmail.com',
-    ];
-    dd($person);
+Route::get("/", function () {
+
+    $aboutPageUrl = route('about');
+    dd($aboutPageUrl);
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::view('/about', 'about')->name('about');
+
+Route::get('{lang}/product/{id}/review/{reviewID}', function (string $lang, string $id, string $reviewId){
+    return "site lang: $lang, product id: $id, reviwer id: $reviewId";
+})->whereIn('lang', ['en', 'ru', 'kz'])->whereNumber('reviewID')->whereAlpha('id');
+
+Route::get('/product/{category?}/', function (string $category = "xyz") {
+    return "Product category: $category";
 });
+
+Route::get("/user/{username}", function (string $username){
+    return "$username";
+})->where("username","[a-z]+");
+
+Route::get("{lang}/product/{id}", function (string $lang, string $id){
+    return "$lang, $id";
+})->where(['lang'=> '[a-z]{2}', 'id'=> '\d{4,}']);
+
+
+Route::get("/search/{search}", function (string $search){
+    return $search;
+})->where('search', '.+');
+
+
+Route::get('/first/{a}/second/{b}', function(float $a, float $b){
+    return $a + $b;
+})->whereNumber(['a','b']);
