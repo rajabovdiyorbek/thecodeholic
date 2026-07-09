dd43839b303f92503ce75799860332a88260e979
Rajabov Diyorbek <rajabov.diyorbek.it@gmail.com>
2026-07-09 19:42:05 +0500

    start to do our project

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,47 @@
 <?php
 
-use App\Http\Controllers\CarController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SumController;
+// use App\Http\Controllers\CarController;
+// use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\SumController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", function () {
 
-    // $aboutPageUrl = route('about');
-    // dd($aboutPageUrl);
-    return view('welcome');
-});
+// Route::get('{lang}/product/{id}/review/{reviewID}', function (string $lang, string $id, string $reviewId){
+//     return "site lang: $lang, product id: $id, reviwer id: $reviewId";
+// })->whereIn('lang', ['en', 'ru', 'kz'])->whereNumber('reviewID')->whereAlpha('id');
 
+// Route::get('/product/{category?}/', function (string $category = "xyz") {
+//     return "Product category: $category";
+// });
+
+// Route::get("/user/{username}", function (string $username){
+//     return "$username";
+// })->where("username","[a-z]+");
+
+// Route::get("{lang}/product/{id}", function (string $lang, string $id){
+//     return "$lang, $id";
+// })->where(['lang'=> '[a-z]{2}', 'id'=> '\d{4,}']);
+
+
+// Route::get("/search/{search}", function (string $search){
+//     return $search;
+// })->where('search', '.+');
+
+
+// Route::get('/first/{a}/second/{b}', function(float $a, float $b){
+//     return $a + $b;
+// })->whereNumber(['a','b']);
+
+// Route::apiResources([
+//     'products'=> ProductController::class,
+//     'cars'=> CarController::class,
+// ]);
+
+// Route::get('/sum/{a}/{b}', [SumController::class, 'sum'])->whereNumber(['a','b']);
+// Route::get('/subtract/{a}/{b}', [SumController::class, 'subtract'])->whereNumber(['a','b']);
+
+
+
+Route::get("/", [HomeController::class,"index"])->name("home");
 Route::view('/about', 'about')->name('about');
-
-Route::get('{lang}/product/{id}/review/{reviewID}', function (string $lang, string $id, string $reviewId){
-    return "site lang: $lang, product id: $id, reviwer id: $reviewId";
-})->whereIn('lang', ['en', 'ru', 'kz'])->whereNumber('reviewID')->whereAlpha('id');
-
-Route::get('/product/{category?}/', function (string $category = "xyz") {
-    return "Product category: $category";
-});
-
-Route::get("/user/{username}", function (string $username){
-    return "$username";
-})->where("username","[a-z]+");
-
-Route::get("{lang}/product/{id}", function (string $lang, string $id){
-    return "$lang, $id";
-})->where(['lang'=> '[a-z]{2}', 'id'=> '\d{4,}']);
-
-
-Route::get("/search/{search}", function (string $search){
-    return $search;
-})->where('search', '.+');
-
-
-Route::get('/first/{a}/second/{b}', function(float $a, float $b){
-    return $a + $b;
-})->whereNumber(['a','b']);
-
-Route::apiResources([
-    'products'=> ProductController::class,
-    'cars'=> CarController::class,
-]);
-
-Route::get('/sum/{a}/{b}', [SumController::class, 'sum'])->whereNumber(['a','b']);
-Route::get('/subtract/{a}/{b}', [SumController::class, 'subtract'])->whereNumber(['a','b']);
